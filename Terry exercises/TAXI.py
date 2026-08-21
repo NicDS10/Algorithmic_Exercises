18886194691c4203c49cf27037a8cc7fbdf5521a
NicDS10 <nicola.delsoldato10@gmail.com>
2026-08-22 01:25:56 +0200

    first exercise

---
+++ Terry exercises/TAXI.py
@@ -0,0 +1,23 @@
+import sys
+from collections import Counter
+
+def main():
+    taxi = []
+    casi_test = tuple(map(int, sys.stdin.read().split()))
+    conta_persone = 0
+    num_persone = -1
+    for indice, caso in enumerate(casi_test[1:]):
+        if conta_persone == num_persone:
+            taxi.append(len(Counter(casi_test[indice - num_persone + 2: indice + 2])))
+            conta_persone = 0
+        elif conta_persone == 0:
+            num_persone = caso
+            conta_persone += 1
+        else:
+            conta_persone += 1
+    return taxi
+
+if __name__ == '__main__':
+    risultati = main()
+    for index, elemento in enumerate(risultati):
+        print(f"Case #{index+1}: {elemento}")
